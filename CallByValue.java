0e50659e9f4bf861bedafa162a053c7eaa450b9c
mazaz <m.alikhwan@stud.uni-hannover.de>
2019-11-02 18:05:11 +0100

    everything works

--- CallByValue.java
+++ CallByValue.java
@@ -3,9 +3,10 @@ public class CallByValue{
 	
 	public static void main(String[]args){
          int a=15,b=20;
+		 System.out.println("a and b before call : " +a +" , " +b);
 		Test ob=new Test();
 		ob.math(15,20);
-		System.out.println("a and b before call : " +a +" , " +b);
+		
 		System.out.println("a and b after call : " +a +" ," +b);
 	}
 }
